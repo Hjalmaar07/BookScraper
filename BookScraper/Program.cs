711a8806e5fde6ccc92fbad9c96eda34c4c0a1d6
Hjalmaar07 <damiank9447@gmail.com>
2024-01-25 19:57:51 +0100

    added dependency injection

--- BookScraper/Program.cs
+++ BookScraper/Program.cs
@@ -1,4 +1,21 @@
-﻿Console.WriteLine("Press Enter to exit...");
+﻿using BookScraper.Domain.Book.Service;
+using BookScraper.Domain.Book.UseCase;
+using BookScraper.Driver.BooksToScrap;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+IHost _host = Host.CreateDefaultBuilder().ConfigureServices(
+    services =>
+    {
+        services.AddSingleton<IBookService, BookService>();
+        services.AddSingleton<IGetBooksUseCase, GetBooksUseCase>();
+        services.AddSingleton<IBooksToScrapRepository, BooksToScrapRepository>();
+    })
+    .Build();
+var app = _host.Services.GetService<IBookService>();
+
+
+Console.WriteLine("Press Enter to exit...");
 ConsoleKeyInfo keyInfo = Console.ReadKey(true);
 if (keyInfo.Key == ConsoleKey.Enter)
 {
